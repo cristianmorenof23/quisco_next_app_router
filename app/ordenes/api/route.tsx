da033c8e2688c2d8e6c9111c881c173a6cfbda78
cristianmorenof23 <crisitan.morenof@hotmail.com>
2024-10-20 11:04:05 -0300

    fix

--- app/ordenes/api/route.tsx
+++ app/ordenes/api/route.tsx
@@ -1,5 +1,8 @@
 import { prisma } from "@/src/lib/prisma";
 
+export const dynamic = 'force-dynamic'
+
+
 export async function GET() {
   const ordenes = await prisma.orden.findMany({
     take: 5,
